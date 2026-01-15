228ba714428185fe9c0165a8d34c1638a26b52be
temi-Dee <ayotemi007@gmail.com>
2026-01-15 23:12:38 +0100

    new update of page

--- week01/scripts/getdates.js
+++ week01/scripts/getdates.js
@@ -1,5 +1,7 @@
-const yearSpan = document.getElementById("year");
-const lastModified = document.getElementById("lastModified");
+// Get the current year and insert it into the footer
+const currentYear = new Date().getFullYear();
+document.getElementById("currentYear").textContent = currentYear;
 
-yearSpan.textContent = new Date().getFullYear();
-lastModified.textContent = `Last Modified: ${document.lastModified}`;
+// Display the document's last modified date
+document.getElementById("lastModified").textContent =
+  "Last Modified: " + document.lastModified;
